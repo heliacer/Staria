92b9421fc4a8fd5b0d639614ee58aa09b8fdcaca
heliacer <heliacer35@gmail.com>
2024-09-15 15:36:24 +0200

    add assets, initial configuration

---
+++ src/assets/wizardPages.ts
@@ -0,0 +1,14 @@
+export const wizardPages = [
+  {
+    title: 'Page 1',
+    description: 'Description of page 1'
+  },
+  {
+    title: 'Page 2',
+    description: 'Description of page 2'
+  },
+  {
+    title: 'Page 3',
+    description: 'Description of page 3'
+  }
+]
